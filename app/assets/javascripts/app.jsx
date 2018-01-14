2715ec17ea3f8d350c19a624501995ab93bb6a3a
Parth Upadhyay <parth.upadhyay@gmail.com>
2018-01-14 15:30:36 +0900

    small fixes

--- app/assets/javascripts/app.jsx
+++ app/assets/javascripts/app.jsx
@@ -4,8 +4,11 @@ import Greeter from './Greeter.jsx';
 
 import '../stylesheets/style.scss'
 
-render((
+render(
+  (
     <div>
         <h1>Playframework, React JS, ES 6 and webpack</h1>
         <Greeter name="Ninja" />
-    </div>), document.getElementById("root"));
+    </div>),
+    document.getElementById("root")
+  );
