9665dfa08a23145dc3a6d6c7180d35cd84e8dcd8
Parth Upadhyay <parth.upadhyay@gmail.com>
2018-01-14 21:21:17 +0900

    Fix Database Definitions

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,31 +2,28 @@
 
 # --- !Ups
 
+CREATE TABLE deck (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE card (
     id bigint(20) NOT NULL AUTO_INCREMENT,
+    deck_id bigint(20) NOT NULL,
     japanese varchar(255) NOT NULL,
     furigana varchar(255) NOT NULL,
     english text NOT NULL,
     notes text NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE review (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
-    card_id bigint(20) NOT NULL REFERENCES card(id),
-    user_id bigint(20) NOT NULL REFERENCES user(id),
-    english_response varchar(255) NOT NULL,
-    japanese_response varchar(255) NOT NULL,
-    english_correct BOOL NOT NULL,
-    japanese_correct BOOL NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (deck_id) REFERENCES deck(id)
 );
 
 # --- !Downs
 
+DROP TABLE deck;
 DROP TABLE card;
-
-DROP TABLE review;
--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -2,20 +2,37 @@
 
 # --- !Ups
 
-CREATE TABLE deck (
+CREATE TABLE review (
     id bigint(20) NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
+    card_id bigint(20) NOT NULL,
+    user_id bigint(20) NOT NULL,
+    english_response varchar(255) NOT NULL,
+    japanese_response varchar(255) NOT NULL,
+    english_correct BOOL NOT NULL,
+    japanese_correct BOOL NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (card_id) REFERENCES card(id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-ALTER TABLE card
-ADD COLUMN deck_id bigint(20) NOT NULL DEFAULT 1;
+
+CREATE TABLE review_statistics (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    card_id bigint(20) NOT NULL REFERENCES card(id),
+    user_id bigint(20) NOT NULL REFERENCES user(id),
+    next_review TIMESTAMP NOT NULL,
+    level tinyint NOT NULL,
+    max_level tinyint NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY (id),
+    FOREIGN KEY (card_id) REFERENCES card(id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    UNIQUE (card_id, user_id)
+);
 
 # --- !Downs
 
-DROP TABLE deck;
-
-ALTER TABLE card
-DROP COLUMN deck_id;
+DROP TABLE review;
+DROP TABLE review_statistics;
